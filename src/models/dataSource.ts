85870dc6b9f8da781500b64c6401ee16691a3b40
CortesWD <cccp.designer@gmail.com>
2022-08-23 12:59:41 -0500

    added models

---
+++ src/models/dataSource.ts
@@ -0,0 +1,36 @@
+import { AssetType } from "./music"
+
+export interface DiscogRes {
+  id: number
+  type: AssetType
+  title: string
+  cover_image: string
+  genre: string[]
+}
+
+export interface SearchResults {
+  results: DiscogRes[]
+}
+
+export interface DiscogMaster {
+  id: number;
+  main_release: number;
+  most_recent_release: number;
+  resource_url: string;
+  uri: string;
+  versions_url: string;
+  main_release_url: string;
+  most_recent_release_url: string;
+  num_for_sale: number;
+  lowest_price: number;
+  images: any[];
+  genres: any[];
+  styles: any[];
+  year: number;
+  tracklist: any[];
+  artists: any[];
+  title: string;
+  data_quality: string;
+  videos: any[];
+  formats?: any[];
+}
