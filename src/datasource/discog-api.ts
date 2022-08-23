6af67f4ed3d4d939066a4c5215c396b3f1fee452
CortesWD <cccp.designer@gmail.com>
2022-08-23 13:00:16 -0500

    added datasource to consume rest api

---
+++ src/datasource/discog-api.ts
@@ -0,0 +1,28 @@
+import { RESTDataSource, RequestOptions } from "apollo-datasource-rest";
+import { DiscogMaster, SearchResults } from "../models/dataSource";
+import { AssetType } from "../models/music";
+
+const DISCOG_URL = 'https://api.discogs.com';
+
+
+class DiscogAPI extends RESTDataSource {
+  constructor() {
+    super();
+    this.baseURL = DISCOG_URL;
+  }
+
+  willSendRequest(request: RequestOptions) {
+    request.headers.set('Authorization', `Discogs key=${process.env.KEY}, secret=${process.env.SECRET}`)
+  }
+
+  resultAlbums(q: string, page: string = "1"): Promise<SearchResults> {
+    return this.get(`/database/search`, { q, page });
+  }
+
+  album(id: number = 1, type: AssetType): Promise<DiscogMaster> {
+    return this.get(`/${type}s/${id}`)
+  }
+
+}
+
+export default DiscogAPI;
