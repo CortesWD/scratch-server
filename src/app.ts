de5812edcccfd8901d6803a789de2dc9e889c563
CortesWD <cccp.designer@gmail.com>
2022-08-31 15:00:22 -0500

    added validation layer

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 /*
  * Dependencies
  */
-import 'dotenv/config'
+import 'dotenv/config';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { readFile } from 'fs/promises';
@@ -22,11 +22,25 @@ const __dirname = path.dirname(__filename);
 
 const MONGODB_URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PWD}@scratch.qatqbxu.mongodb.net/${process.env.MONGO_DB_NAME}`;
 
+const typeDefs = await readFile(path.join(__dirname, '/graphql', 'schema.graphql'), 'utf-8');
+
 const app = express();
 
 app.use(cors(), express.json());
 
-const typeDefs = await readFile(path.join(__dirname, '/graphql', 'schema.graphql'), 'utf-8');
+/*
+ * Controllers
+ */
+app.use('/user', authRoutes);
+
+/*
+ * MW Error
+ */
+app.use((error: any, req: any, res: any, next: any) => {
+  console.log(error);
+  const { statusCode = 500, message, data } = error;
+  res.status(statusCode).json({ message, data });
+});
 
 const apolloServer = new ApolloServer({
   typeDefs,
@@ -36,20 +50,6 @@ const apolloServer = new ApolloServer({
       discogApi: new DiscogAPI()
     }
   },
-});
-
-/*
- * Controllers
- */
-app.use('/user', authRoutes)
-
-/*
- * MW Error
- */
-app.use((error: any, req: any, res: any, next: any) => {
-  console.log(error);
-  const { statusCode = 500, message, data } = error;
-  res.status(statusCode).json({ message, data });
 });
 
 await apolloServer.start();
--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -3,26 +3,65 @@
  */
 import express, { RequestHandler } from 'express';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { validationResult, body } from 'express-validator';
 
 /*
  * Models
  */
-import { User } from '../models/user.js';
+import { iUser, User } from '../models/user.js';
 
 /*
  * Others
  */
 import catchError from '../utils/catchError.js';
+import { CustomError } from '../utils/generic.js';
 
-const login: RequestHandler = (req, res, next) => {
-  const { email, password } = req.body;
-  // const text = (req.body as { text: string }).text;
+const login: RequestHandler = async (req, res, next) => {
+  const { email, password } = (req.body as iUser);
+  const errors = validationResult(req);
+  const invalidCredentials = new CustomError(401, 'invalid credentials');
+
+  try {
+    if (!errors.isEmpty()) {
+      const error = new CustomError(422, 'Validation failed');
+      throw error;
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) { throw invalidCredentials; };
+
+    const matchPwd = await bcrypt.compare(password, user.password);
+
+    if (!matchPwd) { throw invalidCredentials; };
+
+    const token = jwt.sign({
+      email,
+      userId: user._id.toString()
+    }, process.env.JWT_SECRET as string, { expiresIn: '1h' });
+
+    res.status(200)
+      .json({
+        token,
+        userId: user._id.toString()
+      });
+
+  } catch (error) {
+    catchError(error as object, next);
+  }
 }
 
 const signUp: RequestHandler = async (req, res, next) => {
-  const { password, email }: { email: string, password: string } = req.body;
+  const { password, email } = (req.body as iUser);
+  const errors = validationResult(req);
 
   try {
+    if (!errors.isEmpty()) {
+      const error = new CustomError(422, 'Validation failed');
+      throw error;
+    }
+
     const bcPwd = await bcrypt.hash(password, 16);
 
     const user = new User({
@@ -44,9 +83,18 @@ const signUp: RequestHandler = async (req, res, next) => {
 
 };
 
+const passwordValidation = () => {
+  return [
+    body('password', 'invalid password')
+    .isLength({min: 6})
+    .isString()
+    .trim()
+  ]
+}
+
 const router = express.Router();
 
-router.post('/signup', signUp);
-router.post('/access_user', login);
+router.post('/signup', passwordValidation(), signUp);
+router.post('/access_user', passwordValidation(), login);
 
 export default router;
